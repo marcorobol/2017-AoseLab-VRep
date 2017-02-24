d09340da6e44399cf942becb26fcd1149de4d2f8
Marco Robol <marco89mr@gmail.com>
2017-02-24 11:26:10 +0100

    first

---
+++ src/test/App.java
@@ -0,0 +1,15 @@
+package test;
+
+import coppelia.remoteApi;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		remoteApi vrep = new remoteApi();
+		
+		//vrep.simxStart(connectionAddress, connectionPort, waitUntilConnected, doNotReconnectOnceDisconnected, timeOutInMs, commThreadCycleInMs)
+	}
+
+}
