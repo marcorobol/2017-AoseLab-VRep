5aa5078e63748cfd56003536d4e0293ea2aa4381
Marco Robol <marco89mr@gmail.com>
2017-05-16 11:56:13 +0200

    fixes to scripts for linux

--- WDAS JACK Agents/scripts/JDEMacOS.sh
+++ WDAS JACK Agents/scripts/JDEMacOS.sh
@@ -2,4 +2,4 @@ export PATH=$PATH:../../macosx
 export JACK=/Applications/AOS/JACK-56c/lib/jack.jar
 export WDAS=./lib/wdas_api.jar:./lib/wdas_adaptervrep.jar:./lib/wdas_core.jar:./lib/wdas_launcher.jar:./lib/bsh-2.0b6.jar
 export CLASSPATH=$JACK:$WDAS:./bin
-java -classpath $CLASSPATH aos.main.Jack ./src/jack/WDASJackAgents.prj
+java -classpath $CLASSPATH aos.main.Jack ./src/jack/WDASJackAgents.prj 
--- WDAS JACK Agents/scripts/JDElinux32.sh
+++ WDAS JACK Agents/scripts/JDElinux32.sh
@@ -1,5 +1,4 @@
 export PATH=$PATH:../../linux32
-export JACK=/Applications/AOS/JACK-56c/lib/jack.jar
 export WDAS=./lib/wdas_api.jar:./lib/wdas_adaptervrep.jar:./lib/wdas_core.jar:./lib/wdas_launcher.jar:./lib/bsh-2.0b6.jar
-export CLASSPATH=$JACK:$WDAS:./bin
-java -classpath $CLASSPATH aos.main.Jack ./src/jack/WDASJackAgents.prj
+export CLASSPATH=$CLASSPATH:~/AOS/JACK-56c/lib/jack.jar:$WDAS:./bin
+java aos.main.Jack ./src/jack/WDASJackAgents.prj 
